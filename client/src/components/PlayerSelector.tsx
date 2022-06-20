384e8523e6db019c29bed53c96c71964f0eef2ca
DESKTOP-TL38TAD\W <qyqfiles@hotmail.com>
2022-06-20 12:28:08 -0400

    PlayerSelector

--- client/src/components/PlayerSelector.tsx
+++ client/src/components/PlayerSelector.tsx
@@ -1,75 +1,146 @@
 import * as React from 'react';
 import 'assets/PlayerSelector.scss';
 
+export interface Player {
+    name: string;
+    isOnline: boolean;
+    chips: ChipsPocket;
+}
+export interface ChipsPocket {
+    count: Map<ChipsType | string, number>;
+    color: Map<ChipsType | string, ChipsColor>;
+    value: Map<ChipsType | string, ChipsValue>;
+};
+const enum ChipsValue {
+    red = 5,
+    blue = 10,
+    green = 25,
+    black = 100
+}
+const enum ChipsType {
+    red = "red",
+    blue = "blue",
+    green = "green",
+    black = "black"
+}
+const enum ChipsColor {
+    red = "#ff4d4f",
+    blue = "#40a9ff",
+    green = "#73d13d",
+    black = "#141414"
+}
+
+const valueMap: Map<ChipsType, ChipsValue> = new Map([
+    [ChipsType.red, ChipsValue.red],
+    [ChipsType.black, ChipsValue.black],
+    [ChipsType.blue, ChipsValue.blue],
+    [ChipsType.green, ChipsValue.green]
+])
+const colorMap: Map<ChipsType, ChipsColor> = new Map([
+    [ChipsType.red, ChipsColor.red],
+    [ChipsType.black, ChipsColor.black],
+    [ChipsType.blue, ChipsColor.blue],
+    [ChipsType.green, ChipsColor.green]
+])
+
+const chipCounts: Map<ChipsType, number> = new Map([
+    [ChipsType.red, 5],
+    [ChipsType.blue, 5],
+    [ChipsType.green, 5],
+    [ChipsType.black, 5]
+])
+
+export const defaultPocket: ChipsPocket = {
+    count: chipCounts,
+    color: colorMap,
+    value: valueMap
+}
 export interface IPlayerSelectorProps {
 }
 
-export default class PlayerSelector extends React.Component<IPlayerSelectorProps> {
-    private selectAreaRef: React.RefObject<HTMLDivElement>;
+export interface IPlayerSelectorStates {
+    isSectionOpened: boolean;
+    readonly expandedClass: string;
+    readonly hasExpandedClass: string;
+    selectedPlayer: Player | null;
+}
+const players: Array<Player> = [
+    {
+        name: "Diamond Wang",
+        isOnline: false,
+        chips: defaultPocket
+    },
+    {
+        name: "Fragile Flower",
+        isOnline: false,
+        chips: defaultPocket
+    },
+    {
+        name: "Dog Ball",
+        isOnline: false,
+        chips: defaultPocket
+    },
+    {
+        name: "Hillbilly Liu",
+        isOnline: false,
+        chips: defaultPocket
+    }
+];
+
+export default class PlayerSelector extends React.Component<IPlayerSelectorProps, IPlayerSelectorStates> {
+    private wrapperRef: React.RefObject<HTMLDivElement>;
+
     constructor(props: IPlayerSelectorProps) {
         super(props);
-        this.selectAreaRef = React.createRef();
+        this.wrapperRef = React.createRef();
+        this.state = {
+            expandedClass: "is-expanded",
+            hasExpandedClass: "has-expanded-item",
+            isSectionOpened: false,
+            selectedPlayer: null
+        }
     }
 
     componentDidMount() {
-        this.Boxlayout().init();
     }
+    openSection = (event: React.MouseEvent, player: Player) => {
+        event.preventDefault();
+        const { isSectionOpened } = this.state;
+        let element = event.currentTarget as HTMLElement;
+        let wrapper = this.wrapperRef.current!;
 
-    Boxlayout = () => {
-        const wrapper = this.selectAreaRef.current!,
-            sections = [...document.querySelectorAll(".section")],
-            closeButtons = [...document.querySelectorAll(".close-section")],
-            expandedClass = "is-expanded",
-            hasExpandedClass = "has-expanded-item";
-
-        const initEvents = () => {
-            sections.forEach((element) => {
-                element.addEventListener("click", () => openSection(element));
-            });
-            closeButtons.forEach((element) => {
-                element.addEventListener("click", (event) => {
-                    event.stopPropagation();
-                    closeSection(element.parentElement!);
-                });
-            });
-        };
-
-        const openSection = (element: Element) => {
-            if (!element.classList.contains(expandedClass) && wrapper) {
-                element.classList.add(expandedClass);
-                wrapper.classList.add(hasExpandedClass);
-            }
-        };
-
-        const closeSection = (element: Element) => {
-            if (element.classList.contains(expandedClass)) {
-                element.classList.remove(expandedClass);
-                wrapper.classList.remove(hasExpandedClass);
-            }
-        };
-
-        return { init: initEvents };
+        if (!isSectionOpened) {
+            element.classList.add(this.state.expandedClass);
+            wrapper.classList.add(this.state.hasExpandedClass);
+            this.setState({ isSectionOpened: !isSectionOpened });
+        }
+    }
+    closeSection = (event: React.MouseEvent, player: Player) => {
+        event.stopPropagation();
+        const { isSectionOpened } = this.state;
+        let element = event.currentTarget.parentElement as HTMLDivElement;
+        let wrapper = this.wrapperRef.current!;
+        if (isSectionOpened) {
+            element.classList.remove(this.state.expandedClass);
+            wrapper.classList.remove(this.state.hasExpandedClass);
+            this.setState({ isSectionOpened: !isSectionOpened });
+        }
     }
 
     public render() {
         return (
-            <main className="main" ref={this.selectAreaRef}>
-                <section className="section">
-                    <div className='close-section'>&times;</div>
-                    <div className="demo-box">Diamond Wang</div>
-                </section>
-                <section className="section">
-                    <div className="close-section">&times;</div>
-                    <div className="demo-box">Fragile Flower</div>
-                </section>
-                <section className="section">
-                    <div className="close-section">&times;</div>
-                    <div className="demo-box">Dog Ball</div>
-                </section>
-                <section className="section">
-                    <div className="close-section">&times;</div>
-                    <div className="demo-box">Hillbilly Liu</div>
-                </section>
+            <main className="main" ref={this.wrapperRef}>{
+                players.map(player => (
+                    <section
+                        key={player.name}
+                        className="section"
+                        onClick={(e) => this.openSection(e, player)}
+                    >
+                        <div className='close-section' onClick={(e) => this.closeSection(e, player)}>&times;</div>
+                        <div className="demo-box">{player.name}</div>
+                    </section>
+                ))
+            }
             </main>
         );
     }
--- client/src/components/SelectPlayerArea.tsx
+++ client/src/components/SelectPlayerArea.tsx
@@ -1,46 +1,13 @@
 import { Carousel, Slider } from 'antd';
 import 'assets/SelectPlayerArea.scss';
 import React from 'react';
-import PlayerSelector from './PlayerSelector';
+import PlayerSelector, { ChipsPocket, defaultPocket } from './PlayerSelector';
 
 export interface ISelectPlayerAreaProps {
     className?: string;
 }
 export interface ISelectPlayerAreaStates {
-    chipCounts: Map<ChipsType, number>;
 }
-
-interface Player {
-    name: string;
-    isOnline: boolean;
-    chips: ChipsPocket;
-}
-interface ChipsPocket {
-    count: Map<ChipsType | string, number>;
-    color: Map<ChipsType | string, ChipsColor>;
-    value: Map<ChipsType | string, ChipsValue>;
-};
-
-const enum ChipsValue {
-    red = 5,
-    blue = 10,
-    green = 25,
-    black = 100
-}
-const enum ChipsType {
-    red = "red",
-    blue = "blue",
-    green = "green",
-    black = "black"
-}
-const enum ChipsColor {
-    red = "#ff4d4f",
-    blue = "#40a9ff",
-    green = "#73d13d",
-    black = "#141414"
-
-}
-
 
 const marks = {
     0: {
@@ -54,47 +21,23 @@ const marks = {
     }
 }
 export default class SelectPlayerArea extends React.Component<ISelectPlayerAreaProps, ISelectPlayerAreaStates> {
-    readonly valueMap: Map<ChipsType, ChipsValue> = new Map([
-        [ChipsType.red, ChipsValue.red],
-        [ChipsType.black, ChipsValue.black],
-        [ChipsType.blue, ChipsValue.blue],
-        [ChipsType.green, ChipsValue.green]
-    ])
-    readonly colorMap: Map<ChipsType, ChipsColor> = new Map([
-        [ChipsType.red, ChipsColor.red],
-        [ChipsType.black, ChipsColor.black],
-        [ChipsType.blue, ChipsColor.blue],
-        [ChipsType.green, ChipsColor.green]
-    ])
 
     constructor(props: ISelectPlayerAreaProps) {
         super(props);
         this.state = {
-            chipCounts: new Map([
-                [ChipsType.red, 5],
-                [ChipsType.blue, 5],
-                [ChipsType.green, 5],
-                [ChipsType.black, 5]
-            ])
+
         }
     }
 
     onChange = (currentIndex: number) => {
-        console.log(currentIndex);
+        console.log("Carousel switched to index: ", currentIndex);
     }
 
-
     public render() {
-
-        const defaultPocket: ChipsPocket = {
-            count: this.state.chipCounts,
-            color: this.colorMap,
-            value: this.valueMap
-        }
+        const chipsPocket: ChipsPocket = defaultPocket;
         return (
             <Carousel afterChange={this.onChange} >
                 <div >
-                    {/* <PlayerSelector /> */}
                     <div className="carousel-content" >
                         <PlayerSelector />
                     </div>
@@ -102,17 +45,17 @@ export default class SelectPlayerArea extends React.Component<ISelectPlayerAreaP
                 <div>
                     <div className='carousel-content'>
                         {
-                            Array.from(defaultPocket.count).map(([chipType, count]) => (
+                            Array.from(chipsPocket.count).map(([chipType, count]) => (
                                 <Slider
+                                    key={chipType}
                                     className={`slider ${chipType}`}
-                                    trackStyle={{ backgroundColor: defaultPocket.color.get(chipType) }}
-                                    handleStyle={{ borderColor: defaultPocket.color.get(chipType) }}
+                                    trackStyle={{ backgroundColor: chipsPocket.color.get(chipType) }}
+                                    handleStyle={{ borderColor: chipsPocket.color.get(chipType) }}
                                     min={0}
                                     max={10}
-                                    // style={sliderStyle}
                                     vertical
                                     marks={marks}
-                                    defaultValue={defaultPocket.count.get(chipType)}
+                                    defaultValue={chipsPocket.count.get(chipType)}
                                 />
                             ))
                         }
